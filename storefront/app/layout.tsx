95830651f52ad6d6cef9c9038a8912d95ac8379f
kanan06 <kanan@miraclecloud-technology.com>
2026-08-19 13:01:24 +0530

    fix: update GA4 Measurement ID to G-Z2ZLVYGDVG

--- storefront/app/layout.tsx
+++ storefront/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-VG1DPLRRCB');
+            gtag('config', 'G-Z2ZLVYGDVG');
             gtag('config', 'AW-18391804952');
           `
         }} />
